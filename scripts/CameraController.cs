510f08cb76b2be84f9ce024de3842b15b1c0b085
Andre Loewen <Andre010400@web.de>
2026-03-06 22:08:18 +0100

    Fix camera controls

--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -6,27 +6,25 @@ public partial class CameraController : Camera3D
 	[Export] public float MouseSensitivity = 0.15f;
 	[Export] public float FastMultiplier = 3f;
 
-	private float _yaw;
-	private float _pitch;
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
-		_yaw = RotationDegrees.Y;
-		_pitch = RotationDegrees.X;
-		
+		SetInitialRotation();
 		Current = true;
+	}
+
+	private void SetInitialRotation()
+	{
+		Rotation = new Vector3(0, -135, 0);
 	}
 
 	public override void _Input(InputEvent @event)
 	{
 		if (@event.IsActionPressed("ui_cancel"))
 		{
-			if (Input.MouseMode == Input.MouseModeEnum.Captured)
-				Input.MouseMode = Input.MouseModeEnum.Visible;
-			else
-				Input.MouseMode = Input.MouseModeEnum.Captured;
+			Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured
+				? Input.MouseModeEnum.Visible
+				: Input.MouseModeEnum.Captured;
 		}
 
 		if (Input.MouseMode != Input.MouseModeEnum.Captured)
@@ -34,34 +32,30 @@ public partial class CameraController : Camera3D
 
 		if (@event is InputEventMouseMotion mouseMotion)
 		{
-			_yaw -= mouseMotion.Relative.X * MouseSensitivity;
-			_pitch -= mouseMotion.Relative.Y * MouseSensitivity;
-			_pitch = Mathf.Clamp(_pitch, -89f, 89f);
-
-			RotationDegrees = new Vector3(_pitch, _yaw, 0f);
+			RotationDegrees -= new Vector3(mouseMotion.Relative.Y, mouseMotion.Relative.X, 0) * MouseSensitivity;
 		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{		
+	{
 		float speed = MoveSpeed;
+
 		if (Input.IsActionPressed("ui_shift"))
-		{
 			speed *= FastMultiplier;
-		}
 
 		Vector3 direction = Vector3.Zero;
+
 		if (Input.IsActionPressed("ui_up")) direction -= Transform.Basis.Z;
 		if (Input.IsActionPressed("ui_down")) direction += Transform.Basis.Z;
 		if (Input.IsActionPressed("ui_left")) direction -= Transform.Basis.X;
 		if (Input.IsActionPressed("ui_right")) direction += Transform.Basis.X;
-		if (Input.IsActionPressed("ui_page_up")) direction -= Transform.Basis.Y;
-		if (Input.IsActionPressed("ui_page_down")) direction += Transform.Basis.Y;
+		if (Input.IsActionPressed("ui_page_up")) direction += Transform.Basis.Y;
+		if (Input.IsActionPressed("ui_page_down")) direction -= Transform.Basis.Y;
 
 		if (direction != Vector3.Zero)
 		{
-			Position += direction.Normalized() * speed * (float)delta;
+			direction = direction.Normalized();
+			Position += direction * speed * (float)delta;
 		}
 	}
 }
